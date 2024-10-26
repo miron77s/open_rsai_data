8611d4ebe77879114cae5b45af92a8520d1cc82d
Sergey Miroshnichenko <s.miroshnichenko@devar.org>
2024-10-26 17:15:09 +0300

    Initial

---
+++ download.sh
@@ -0,0 +1,40 @@
+mkdir -p ./hydro/markup/
+wget -nc -O ./hydro/markup/water_all.zip.001      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/rvSGRVhvgVWpBg
+wget -nc -O ./hydro/markup/water_all.zip.002      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/qUGGGKzNB_p04Q
+wget -nc -O ./hydro/markup/water_all.zip.003      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/kmxv5VateVIfmg
+wget -nc -O ./hydro/markup/water_all.zip.004      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/N6aX8lYmxWrZXA
+wget -nc -O ./hydro/markup/water_all.zip.005      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/YDWLEkk9bsSrkw
+wget -nc -O ./hydro/markup/water_all.zip.006      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/9tOcFLNwlh9lww
+wget -nc -O ./hydro/markup/water_all.zip.007      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/gge6Y7upFnM55w
+wget -nc -O ./hydro/markup/water_all.zip.008      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/aZwFavVrNTbacA
+wget -nc -O ./hydro/markup/water_all.zip.009      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/rpyFmUPdgIH02A
+7z x ./hydro/markup/water_all.zip.001 -o./hydro/markup/
+rm ./hydro/markup/water_all.zip.001 ./hydro/markup/water_all.zip.002 ./hydro/markup/water_all.zip.003
+rm ./hydro/markup/water_all.zip.004 ./hydro/markup/water_all.zip.005 ./hydro/markup/water_all.zip.006
+rm ./hydro/markup/water_all.zip.007 ./hydro/markup/water_all.zip.008 ./hydro/markup/water_all.zip.009
+
+mkdir -p ./green/markup/
+wget -nc -O ./green/markup/vegetation_all.zip.001 https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/oPqCjlF-oE1xfA
+wget -nc -O ./green/markup/vegetation_all.zip.002 https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/IXbJLTgbrrBwqQ
+wget -nc -O ./green/markup/vegetation_all.zip.003 https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/VZRnR8i5vIu6sg
+wget -nc -O ./green/markup/vegetation_all.zip.004 https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/335RvStwsebR8Q
+wget -nc -O ./green/markup/vegetation_all.zip.005 https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/rc9DgaIpPN289w
+wget -nc -O ./green/markup/vegetation_all.zip.006 https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/HHa6yI76MlewVA
+7z x ./green/markup/vegetation_all.zip.001 -o./green/markup/
+rm ./green/markup/vegetation_all.zip.001 ./green/markup/vegetation_all.zip.002 ./green/markup/vegetation_all.zip.003
+rm ./green/markup/vegetation_all.zip.004 ./green/markup/vegetation_all.zip.005 ./green/markup/vegetation_all.zip.006
+
+mkdir -p ./buildings/markup/
+wget -nc -O ./buildings/markup/buildings-yolo.tar https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/OhGwDv_Stlo-LQ
+tar -xf ./buildings/markup/buildings-yolo.tar -C ./buildings/markup/
+rm ./buildings/markup/buildings-yolo.tar
+
+mkdir -p ./raster/Kursk/
+wget -nc -O ./raster/Kursk/Bing_19_3395.tif       https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/st-_Ec9PuCWd1A
+wget -nc -O ./raster/Kursk/Bing_19_3395.tif.ovr   https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/yOL4RVDyx_Srww
+wget -nc -O ./raster/Kursk/Google_19_3395.tif     https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/CMFoAK8t44FxHg
+wget -nc -O ./raster/Kursk/Google_19_3395.tif.ovr https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/2b7E6UHl59XN-Q
+wget -nc -O ./raster/Kursk/Mapbox_19_3395.tif     https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/4Ve-IcqCMknVhA
+wget -nc -O ./raster/Kursk/Mapbox_19_3395.tif.ovr https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/8zsxFvPlnNXHww
+wget -nc -O ./raster/Kursk/Yandex_19_3395.tif     https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/8C5hMaDDzUqZvw
+wget -nc -O ./raster/Kursk/Yandex_19_3395.tif.ovr https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/hRvIBANIo03BqA
