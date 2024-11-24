565a30c243cda2c82be0cca8cd81020f002c64f7
Sergey Miroshnichenko <s.miroshnichenko@devar.org>
2024-11-24 13:39:47 +0300

    Updated hydro and green dataset links

--- download_hydro_dataset.sh
+++ download_hydro_dataset.sh
@@ -1,13 +1,13 @@
 mkdir -p ./hydro/markup/
-wget -nc -O ./hydro/markup/water_all.zip.001      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/rvSGRVhvgVWpBg
-wget -nc -O ./hydro/markup/water_all.zip.002      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/qUGGGKzNB_p04Q
-wget -nc -O ./hydro/markup/water_all.zip.003      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/kmxv5VateVIfmg
-wget -nc -O ./hydro/markup/water_all.zip.004      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/N6aX8lYmxWrZXA
-wget -nc -O ./hydro/markup/water_all.zip.005      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/YDWLEkk9bsSrkw
-wget -nc -O ./hydro/markup/water_all.zip.006      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/9tOcFLNwlh9lww
-wget -nc -O ./hydro/markup/water_all.zip.007      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/gge6Y7upFnM55w
-wget -nc -O ./hydro/markup/water_all.zip.008      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/aZwFavVrNTbacA
-wget -nc -O ./hydro/markup/water_all.zip.009      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/rpyFmUPdgIH02A
+wget -nc -O ./hydro/markup/water_all.zip.001      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/ThHX4uln7NcUbA
+wget -nc -O ./hydro/markup/water_all.zip.002      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/paKh94P1korfyg
+wget -nc -O ./hydro/markup/water_all.zip.003      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/Du2pi-SBYT2bMA
+wget -nc -O ./hydro/markup/water_all.zip.004      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/czXcGScGuzu8qw
+wget -nc -O ./hydro/markup/water_all.zip.005      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/NkwhDWVxqN54vw
+wget -nc -O ./hydro/markup/water_all.zip.006      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/q9FhnookKNsW7Q
+wget -nc -O ./hydro/markup/water_all.zip.007      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/plo1Ne5BPhhuFQ
+wget -nc -O ./hydro/markup/water_all.zip.008      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/wbpsQfRXlLENrQ
+wget -nc -O ./hydro/markup/water_all.zip.009      https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/9EuIg6QNyP4X2Q
 7z x ./hydro/markup/water_all.zip.001 -o./hydro/markup/
 rm ./hydro/markup/water_all.zip.001 ./hydro/markup/water_all.zip.002 ./hydro/markup/water_all.zip.003
 rm ./hydro/markup/water_all.zip.004 ./hydro/markup/water_all.zip.005 ./hydro/markup/water_all.zip.006
